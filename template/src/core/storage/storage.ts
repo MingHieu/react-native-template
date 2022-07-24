cd14277a8e841f2aa4be1ccb7a2fb462733e7333
MingHieu <hieubeo0612@gmail.com>
2022-07-25 00:21:46 +0700

    Config TypeScript

--- template/src/core/storage/storage.ts
+++ template/src/core/storage/storage.ts
@@ -1,10 +1,8 @@
 import {getData, setData, removeData} from '~core/storage/storage.service';
-
-const USER_TOKEN = 'user_token';
+import {USER_TOKEN} from './constant';
 
 export const UserTokenStorage = {
   set: value => setData(USER_TOKEN, value),
   get: () => getData(USER_TOKEN),
   remove: () => removeData(USER_TOKEN),
 };
-
