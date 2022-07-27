1efed704467df282ebb44b3dd5f24a581c4222d2
MingHieu <hieubeo0612@gmail.com>
2022-07-27 11:09:55 +0700

    Add react-reanimated and react-gesture-handler

--- template/App.tsx
+++ template/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
+import '~core/i18n'; // init I18n
 import {store} from '~core/redux/store';
 import AppRouter from '~router';
-import '~core/i18n'; // init I18n
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </GestureHandlerRootView>
   );
 };
 
