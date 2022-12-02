d6ad73ae06ae5e2c3ce2cf48fbb887a20b53870e
MingHieu <77161145+MingHieu@users.noreply.github.com>
2022-12-02 23:20:54 +0700

    add svg note

--- template/src/assets/svg/index.ts
+++ template/src/assets/svg/index.ts
@@ -1,3 +1,9 @@
 import LeftArrow from './LeftArrow.svg';
 
 export {LeftArrow};
+
+/**
+ * Note : to colozie your svg, 
+ * change all path from svg file to currentColor 
+ * Ex : <path fill="currentColor"/>
+ */
