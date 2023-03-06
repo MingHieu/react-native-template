fb81cdc73bbe53161bfc5c55334e684d587ba326
MingHieu <77161145+MingHieu@users.noreply.github.com>
2023-03-06 23:07:31 +0700

    update input component

--- template/src/shared/components/Input.tsx
+++ template/src/shared/components/Input.tsx
@@ -1,16 +1,19 @@
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import React, {
   forwardRef,
   useImperativeHandle,
   useRef,
   useState,
 } from 'react';
-import { useAppSelector } from '~shared/hooks';
+import { useTheme } from '~shared/hooks';
 import { AppText } from './AppText';
 import { COLOR } from '~shared/theme';
 
 interface IProps {
   label: string;
+  icon?: React.ReactElement;
+  onIconPress?: () => void;
+  secure?: boolean;
 }
 
 export interface CustomInputHandle {
@@ -18,58 +21,71 @@ export interface CustomInputHandle {
   getValue(): string;
 }
 
-export const CustomInput = forwardRef<CustomInputHandle, IProps>((props, ref) => {
-  const { label } = props;
-  const { color } = useAppSelector((state) => state.theme);
-  const [invalid, setInvalid] = useState(false);
-  const [invalidText, setInvalidText] = useState<string>();
-  const inputRef = useRef<string>('');
+export const CustomInput = forwardRef<CustomInputHandle, IProps>(
+  (props, ref) => {
+    const { label, icon, onIconPress, secure } = props;
+    const { color } = useTheme();
+    const [invalid, setInvalid] = useState(false);
+    const [invalidText, setInvalidText] = useState<string>();
+    const inputRef = useRef<string>('');
 
-  useImperativeHandle(ref, () => ({ setStatus, getValue }), []);
+    useImperativeHandle(ref, () => ({ setStatus, getValue }), []);
 
-  const setStatus: CustomInputHandle['setStatus'] = (status, text) => {
-    switch (status) {
-      case 'normal':
-        setInvalid(true);
-        setInvalidText(text);
-        break;
+    const setStatus: CustomInputHandle['setStatus'] = (status, text) => {
+      switch (status) {
+        case 'normal':
+          setInvalid(false);
+          setInvalidText('');
+          break;
 
-      case 'invalid':
-        setInvalid(false);
-        setInvalidText('');
-        break;
-    }
-  };
+        case 'invalid':
+          setInvalid(true);
+          setInvalidText(text);
+          break;
+      }
+    };
 
-  const getValue = () => {
-    return inputRef.current;
-  };
+    const getValue = () => {
+      return inputRef.current;
+    };
 
-  return (
-    <View style={styles.container}>
-      <AppText style={{ color: invalid ? COLOR.red : color.primary }}>
-        {label}
-      </AppText>
-      <TextInput
-        onChangeText={(text) => (inputRef.current = text)}
-        style={{
-          ...styles.input,
-          borderBottomColor: invalid ? COLOR.red : '#CDD1D0',
-        }}
-      />
-      {invalid && <AppText style={styles.invalidText}>{invalidText}</AppText>}
-    </View>
-  );
-});
+    return (
+      <View style={styles.container}>
+        <AppText style={{ color: invalid ? COLOR.red : color.on_surface }}>
+          {label}
+        </AppText>
+        <View
+          style={{
+            ...styles.inputContainer,
+            borderBottomColor: invalid ? COLOR.red : '#CDD1D0',
+          }}
+        >
+          <TextInput
+            onChangeText={(text) => (inputRef.current = text)}
+            style={{ ...styles.input, paddingRight: icon ? 4 : 0 }}
+            secureTextEntry={secure}
+          />
+          <TouchableOpacity onPress={onIconPress}>{icon}</TouchableOpacity>
+        </View>
+        {invalid && <AppText style={styles.invalidText}>{invalidText}</AppText>}
+      </View>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
     marginBottom: 30,
   },
-  input: {
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderBottomWidth: 1,
-    paddingVertical: 6,
     marginTop: 2,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 6,
   },
   invalidText: {
     marginTop: 8,
